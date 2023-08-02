789fc8327642d5573519b978c1c30e0d61a7915f
Jad Bizri <91910175+JadBizri@users.noreply.github.com>
2023-08-02 11:37:26 -0400

    Add isbn attribute to Book object

--- src/main/java/com/jadb/bookstore/managementsystem/bookstore/Product/Book.java
+++ src/main/java/com/jadb/bookstore/managementsystem/bookstore/Product/Book.java
@@ -6,6 +6,7 @@ import jakarta.persistence.Entity;
 @Entity
 public class Book extends Product {
 
+    private String isbn; //to hold book isbn
     /*
         Constructor that accepts arguments for the book's name, available number, author's name, price, and year published
         @param int books The number of books currently available
@@ -14,15 +15,24 @@ public class Book extends Product {
         @param String authorName The name of the book's author
         @param int year The year the book was published
     */
-    public Book(long id, String name, double price, int copies, String authorName, int year){
+    public Book(long id, String name, double price, int copies, String authorName, int year, String isbn){
         super(id, name, price, copies, authorName, year);
+        this.isbn = isbn;
     }
 
-    public Book(String name, double price, int copies, String authorName, int year){
+    public Book(String name, double price, int copies, String authorName, int year, String isbn){
         super(name, price, copies, authorName, year);
+        this.isbn = isbn;
     }
 
-    public Book() {
+    //Empty Constructor
+    public Book() {}
 
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
     }
 }
