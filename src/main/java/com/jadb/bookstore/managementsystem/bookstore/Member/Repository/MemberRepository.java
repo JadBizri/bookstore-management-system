eb20a37a8c2e843ec61be8e684be092e61d95c4f
Jad Bizri <91910175+JadBizri@users.noreply.github.com>
2023-08-02 12:10:36 -0400

    Implement member config interface

--- src/main/java/com/jadb/bookstore/managementsystem/bookstore/Member/Repository/MemberRepository.java
+++ src/main/java/com/jadb/bookstore/managementsystem/bookstore/Member/Repository/MemberRepository.java
@@ -1,4 +1,10 @@
 package com.jadb.bookstore.managementsystem.bookstore.Member.Repository;
 
-public interface MemberRepository {
+import com.jadb.bookstore.managementsystem.bookstore.Member.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MemberRepository extends JpaRepository<Member, Long> {
+
 }
