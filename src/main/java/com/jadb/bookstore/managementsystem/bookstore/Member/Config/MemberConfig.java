9c235e4e861faa2437343defd09a78f092dc79f3
Jad Bizri <91910175+JadBizri@users.noreply.github.com>
2023-08-02 12:31:40 -0400

    Implement member config class

--- src/main/java/com/jadb/bookstore/managementsystem/bookstore/Member/Config/MemberConfig.java
+++ src/main/java/com/jadb/bookstore/managementsystem/bookstore/Member/Config/MemberConfig.java
@@ -1,4 +1,46 @@
 package com.jadb.bookstore.managementsystem.bookstore.Member.Config;
 
+import com.jadb.bookstore.managementsystem.bookstore.Member.Member;
+import com.jadb.bookstore.managementsystem.bookstore.Member.PremiumMember;
+import com.jadb.bookstore.managementsystem.bookstore.Member.Repository.MemberRepository;
+import com.jadb.bookstore.managementsystem.bookstore.Product.Book;
+import com.jadb.bookstore.managementsystem.bookstore.Product.CD;
+import com.jadb.bookstore.managementsystem.bookstore.Product.DVD;
+import com.jadb.bookstore.managementsystem.bookstore.Product.Product;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
+
+@Configuration
 public class MemberConfig {
+
+    @Bean
+    CommandLineRunner commandLineRunner2(
+            MemberRepository repository) {
+        return args -> {
+            Member tiffany = new Member(
+                    "Tiffany",
+                    "Miles",
+                    3,
+                    2,
+                    1
+            );
+
+            PremiumMember jorge = new PremiumMember(
+                    "Jorge",
+                    "Jones",
+                    7,
+                    6,
+                    5,
+                    "Visa",
+                    true
+            );
+
+            repository.saveAll(
+                    List.of(tiffany, jorge)
+            );
+        };
+    }
 }
