81eea569d0a6c3dd0c867b6a39f1edea1d4b8b77
Jad Bizri <91910175+JadBizri@users.noreply.github.com>
2023-08-02 12:19:43 -0400

    Implement service and controller class for Member

--- src/main/java/com/jadb/bookstore/managementsystem/bookstore/Member/Controller/MemberController.java
+++ src/main/java/com/jadb/bookstore/managementsystem/bookstore/Member/Controller/MemberController.java
@@ -1,4 +1,24 @@
 package com.jadb.bookstore.managementsystem.bookstore.Member.Controller;
 
+import com.jadb.bookstore.managementsystem.bookstore.Member.Member;
+import com.jadb.bookstore.managementsystem.bookstore.Member.Service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(path = "api/v1/members")
 public class MemberController {
+
+    private final MemberService memberService;
+
+    @Autowired
+    public MemberController(MemberService memberService){this.memberService = memberService;}
+
+    @GetMapping
+    public List<Member> getMembers(){return memberService.getMembers();
+    }
 }
--- src/main/java/com/jadb/bookstore/managementsystem/bookstore/Member/Service/MemberService.java
+++ src/main/java/com/jadb/bookstore/managementsystem/bookstore/Member/Service/MemberService.java
@@ -1,4 +1,25 @@
 package com.jadb.bookstore.managementsystem.bookstore.Member.Service;
 
+import com.jadb.bookstore.managementsystem.bookstore.Member.Member;
+import com.jadb.bookstore.managementsystem.bookstore.Member.Repository.MemberRepository;
+import com.jadb.bookstore.managementsystem.bookstore.Product.Product;
+import com.jadb.bookstore.managementsystem.bookstore.Product.Repositories.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class MemberService {
+
+    private final MemberRepository memberRepository;
+
+    @Autowired
+    public MemberService(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
+    public List<Member> getMembers(){
+        return memberRepository.findAll();
+    }
 }
