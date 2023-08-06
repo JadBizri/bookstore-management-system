23ed8dd1ae9d213f29b91313e1a1d80689eee6bf
Jad Bizri <91910175+JadBizri@users.noreply.github.com>
2023-08-06 15:44:50 -0400

    Add getter/setter for email

--- src/main/java/com/jadb/bookstore/managementsystem/bookstore/Member/Member.java
+++ src/main/java/com/jadb/bookstore/managementsystem/bookstore/Member/Member.java
@@ -143,6 +143,14 @@ public class Member{
         this.DVDsBought = DVDsBought;
     }
 
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
     @Override
     public String toString() {
         return "ID " + id + ": " + lName + ", " + fName;
