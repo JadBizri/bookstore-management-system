3a831615bcc9b59d62aaa0740557befc48ff1950
Jad Bizri <91910175+JadBizri@users.noreply.github.com>
2023-08-09 15:19:06 -0400

    Optimize, clean up, and add more comments to code

--- src/main/resources/static/script.js
+++ src/main/resources/static/script.js
@@ -1,10 +1,14 @@
+//where rows will be inserted
 const productTableBody = document.getElementById('productTableBody');
-const productsSelect = document.getElementById('products');
-const sortSelect = document.getElementById('sort');
 
+const productsSelect = document.getElementById('products'); //show drop down
+const sortSelect = document.getElementById('sort'); //sort drop down
+
+//update table on any change of any drop down
 productsSelect.addEventListener('change', updateTable);
 sortSelect.addEventListener('change', updateTable);
 
+//function to update the table based on selected options
 function updateTable() {
 
     // Clear the existing table rows
@@ -14,78 +18,78 @@ function updateTable() {
     let selectedProduct = productsSelect.value;
     let selectedSort = sortSelect.value;
 
-    if (selectedProduct === 'All' && selectedSort === 'Default') {
-        fetchAllProducts();
-        return;
-    }
     fetchSortedProducts(selectedProduct, selectedSort);
 }
 
+//function to insert a new row into the product table with product data
 function insertProductRow(product, rows) {
+    //new row
     const row = productTableBody.insertRow();
+
+    //cells for each column
     const nameCell = row.insertCell(0);
     const priceCell = row.insertCell(1);
     const creatorCell = row.insertCell(2);
     const typeCell = row.insertCell(3);
     const copiesCell = row.insertCell(4);
 
+    //populate cells
     nameCell.textContent = product.name;
     priceCell.textContent = `$${product.price.toFixed(2)}`;
     creatorCell.textContent = product.creator;
     copiesCell.textContent = product.numCopies;
     typeCell.textContent = product.type;
 
-    // Add the row to the 'rows' array
+    //add the row to the 'rows' array for later reference
     rows.push(row);
 
+    //add a click event listener to highlight the clicked row
     row.addEventListener('click', () => {
-        // Remove highlight from previously selected rows
+        //remove highlight from previously selected rows
         rows.forEach(r => r.classList.remove('selected'));
 
-        // Add highlight to the clicked row
+        //add highlight to the clicked row
         row.classList.add('selected');
     });
 }
 
+//function to fetch data from the provided URL and handle success or error
 function fetchData(url, successCallback, errorCallback) {
+    //fetch data from the given URL using the Fetch API
     fetch(url)
-        .then(response => response.json())
+        .then(response => response.json()) //parse response as JSON
         .then(data => {
-            successCallback(data); // Call the success callback with the fetched data
+            successCallback(data); //call the success callback with the fetched data
         })
         .catch(error => {
-            errorCallback(error); // Call the error callback with the error object
+            errorCallback(error); //call the error callback with the error object
         });
 }
 
-function fetchAllProducts() {
-    fetchData('/api/v1/products',
-        data => {
-            const rows = []; // Define the 'rows' array to store row elements
-            data.forEach(product => {
-                insertProductRow(product, rows);
-            });
-        },
-        error => {
-            // Handle error
-            console.error('Error fetching data:', error);
-        }
-    );
-}
-
+//function to fetch sorted products optionally based on type and sorting criteria
 function fetchSortedProducts(type, sortBy) {
-    fetchData(`/api/v1/products/sort?type=${type}&sort=${sortBy}`,
+
+    //construct the URL with the provided type and sortBy parameters
+    const url = `/api/v1/products/sort?type=${type}&sort=${sortBy}`;
+
+    //use the fetchData function to fetch data from the constructed URL
+    fetchData(url,
         data => {
-            const rows = []; // Define the 'rows' array to store row elements
+            const rows = []; //define the 'rows' array to store row elements
+
+            //for each product, insert it in a row
             data.forEach(product => {
                 insertProductRow(product, rows);
             });
         },
         error => {
-            // Handle error
+            //handle error by logging an error message
             console.error('Error fetching data:', error);
         }
     );
 }
 
-updateTable();
+//on load up, update table
+window.onload = function() {
+    updateTable();
+};
