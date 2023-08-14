e75122e5b285718864ebf593413e0331d2424443
Jad Bizri <91910175+JadBizri@users.noreply.github.com>
2023-08-13 20:30:17 -0400

    Optimize imports

--- src/test/java/com/jadb/bookstore/managementsystem/bookstore/ProductControllerTest.java
+++ src/test/java/com/jadb/bookstore/managementsystem/bookstore/ProductControllerTest.java
@@ -3,7 +3,6 @@ package com.jadb.bookstore.managementsystem.bookstore;
 import com.jadb.bookstore.managementsystem.bookstore.Product.Book;
 import com.jadb.bookstore.managementsystem.bookstore.Product.CD;
 import com.jadb.bookstore.managementsystem.bookstore.Product.DVD;
-import com.jadb.bookstore.managementsystem.bookstore.Product.Product;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
